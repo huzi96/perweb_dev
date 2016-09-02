2ceedac51f7288f432e7eaaaf101037d8cf4938f
huzi96 <huyy@pku.edu.cn>
2016-09-02 16:35:05 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by huyueyu on 16/8/30.
+ */
+$(document).ready(function(){
+    $('.card-image').addClass('waves-effect');
+    $(document).ready(function(){
+        $('.scrollspy').scrollSpy();
+    });
+});
